25d00f0a9a9a982bd7cc6d4c8754a8abe5824ef0
Dominic Szablewski <dominic@phoboslab.org>
2024-08-22 22:14:53 +0200

    Force char to unsigned for consistent hashes across platforms; close #4

--- qop.h
+++ qop.h
@@ -135,7 +135,7 @@ typedef unsigned long long qop_uint64_t;
 static inline qop_uint64_t qop_hash(const char *key) {
 	qop_uint64_t h = 525201411107845655ull;
 	for (;*key;++key) {
-		h ^= *key;
+		h ^= (unsigned char)*key;
 		h *= 0x5bd1e9955bd1e995ull;
 		h ^= h >> 47;
 	}
